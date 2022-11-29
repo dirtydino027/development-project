7a934f2ad658fa12e5f471d4a2292fc6c2c00ab8
dirtydino027 <114872234+dirtydino027@users.noreply.github.com>
2022-11-29 12:04:52 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,16 +1,21 @@
-// TODO: create a component that displays a single bakery item
+import "./BakeryItem.css";
 import addItem from "../App.js"
 import setCartItems from "../App.js"
 import cartItems from "../App.js"
 export default function BakeryItem(props) {
   const item = props.item;
   return(
-    <div>
-      <p>{item.name}</p>
-      <p>{item.description}</p>
-      <p>{item.price}</p>
-      <img src={item.image}></img>
-      <button onClick={() => props.setCartItems(item)}>add2cart</button>
+    <div id="bakeryItemContainer">
+      <div class="field" id="itemName">{item.name}</div>
+      <div class="field">{item.description}</div>
+      <div class="field">color: {item.color}</div>
+      <div class="field">type: {item.type}</div>
+      <img class="field" src={item.image}></img>
+      <div class="field">${item.price}</div>
+      <div class="field" id="buttonDiv"> 
+        <button class="addRemoveButtons" onClick={() => props.addCartItem(item)}>add2cart</button>
+        <button class="addRemoveButtons" onClick={() => props.removeCartItem(item)}>remove!!</button>
+      </div>
     </div>
   );
 }
